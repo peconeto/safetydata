6f0c478a37a534f859c807a95c05cf170f39100e
Péricles Maranhão Neto <peconeto@gmail.com>
2017-06-20 17:39:34 -0400

    Improved spcChart() function to allow for both np and p charts

--- R/spcChart.R
+++ R/spcChart.R
@@ -1,8 +1,8 @@
 #' @title
-#' Statistical Process Control np-Chart Using ggplot2
+#' Statistical Process Control np- or p-Chart Using ggplot2
 #' @description
-#' Function to create statistical process control (SPC) np-chart from aviation safety datasets.
-#' Uses qcc:qcc() to generate np-chart values and ggplot2 to generate the plot.
+#' Function to create statistical process control (SPC) np-chart or p-chart from aviation safety datasets.
+#' Uses qcc:qcc() to generate SPC chart values and ggplot2 to generate the plot.
 #' @param filename Name of CSV file on working directory to be consumed; expects specific formatting:
 #' First column contains dates in the format "Mmm, YYYY"
 #' Second column contains a count of events/issues/deficiencies
@@ -10,6 +10,7 @@
 #' There should be no skipped months within dataset
 #' Ordered from oldest date first (top) to newest date last (bottom)
 #' @param title Optional plot title
+#' @param type Chart type; accepts np or p, default np
 #' @param group Date grouping for data to be plotted; accepts monthly or quarterly, default monthly
 #' @param enddate Ending date ; accepts lubridate functions, default last day of previous month from current date
 #' @param startdate Starting date for data to be plotted; accepts lubridate functions, default first day 24 months before current date
@@ -21,17 +22,26 @@
 #' @export
 spcChart <- function(filename,
                      title = NA,
+                     type = c("np", "p"),
                      group = c("monthly", "quarterly"),
                      enddate = floor_date(today(), "month") - 1, # Last day of previous month
                      startdate = enddate - months(24) + 1) { # Go 24 months back
-
+  
+  # Remove warnings
+  options(warn = -1)
+  
+  # Check for filename
+  if(is.null(filename)) {
+    stop("Filename argument is required")
+  }
+  
   # Required packages without exposing messages
   suppressMessages(require(lubridate))
   suppressMessages(require(tidyverse))
   suppressMessages(require(qcc))
-
+  
   # Read input files
-  df <- read.csv(filename, stringsAsFactors = FALSE)
+  df <- suppressMessages(read_csv(filename))
 
   # Check for legitimate arguments
   if(!(is.data.frame(df)) | ncol(df) != 3) {
@@ -43,9 +53,12 @@ spcChart <- function(filename,
   if(!is.Date(startdate) | !(is.Date(enddate))) {
     stop("Start and end dates not recognized")
   }
+  if(!type[1] %in% c("np", "p")) {
+    stop("Only np or p chart types accepted")
+  }
 
   # Clean up input data file
-  # Fix column names based on position
+  # [Re-]name columns based on position
   df <- df %>%
     select(Date_Label_Month = 1,
            Events = 2,
@@ -78,7 +91,7 @@ spcChart <- function(filename,
                                       Year_int, sep = ", ")) %>%
     select(-Quarter_int, -Year_int)
 
-  # Apply filter
+  # Apply date filter
   df <- df %>%
     filter(Date_Collected >= startdate, Date_Collected <= enddate)
 
@@ -139,8 +152,15 @@ spcChart <- function(filename,
     unlist() %>%
     unname()
 
+  # Chart type
+  chartType <- type[1]
+  centerLabel <- case_when(chartType == "np" ~ "Average Events Adjusted for Flights",
+                           chartType == "p" ~ "Average Events")
+  yAxisLabel <- case_when(chartType == "np" ~ "Count of Events",
+                           chartType == "p" ~ "Proportion of Events")
+  
   # Calculate QCC values
-  qcc_calc <- qcc(data = observations, type = "np", sizes = sizes, plot = FALSE)
+  qcc_calc <- qcc(data = observations, type = chartType, sizes = sizes, plot = FALSE)
 
   qcc_df <- data.frame(labels = labels,
                            center = qcc_calc$center,
@@ -162,9 +182,17 @@ spcChart <- function(filename,
   beyond_limits_index <- unname(unlist(qcc_calc$violations[1]))
   qcc_df[beyond_limits_index, "beyond_limits"] <- 1
 
+  # scale_color_manual values
+  if(chartType == "np") {
+    scale_values <- c("Control Limits Adjusted for Flights" = "red", "Average Events Adjusted for Flights" = "blue", "Count of Events" = "black")
+  }
+  if(chartType == "p") {
+    scale_values <- c("Control Limits Adjusted for Flights" = "red", "Average Events" = "blue", "Proportion of Events" = "black")
+  }
+  
   # Create ggplot
   plot <- ggplot(qcc_df) +
-    labs(title = title, x = "Dates", y = "Count") +
+    labs(title = title, x = "Dates", y = yAxisLabel[1]) +
     theme(axis.text = element_text(size = 8),
           axis.title = element_text(size = 14),
           title = element_text(size = 18),
@@ -172,12 +200,15 @@ spcChart <- function(filename,
           legend.position = "bottom") +
     geom_step(aes(x = labels, y = LCL, color = "Control Limits Adjusted for Flights"), linetype = 2, group = 1) +
     geom_step(aes(x = labels, y = UCL, color = "Control Limits Adjusted for Flights"), linetype = 2, group = 1) +
-    geom_step(aes(x = labels, y = center, color = "Average Events Adjusted for Flights"), linetype = 2, group = 1) +
-    geom_line(aes(x = labels, y = values, color = "Count of Events"), group = 1) +
+    geom_step(aes(x = labels, y = center, color = centerLabel[1]), linetype = 2, group = 1) +
+    geom_line(aes(x = labels, y = values, color = yAxisLabel[1]), group = 1) +
     geom_point(aes(x = labels, y = values, fill = factor(beyond_limits)), shape = 21 , color = "transparent" , size = 4) +
     scale_fill_manual(values = c("0" = "black", "1" = "red", "2" = "yellow"), guide = FALSE) +
-    scale_color_manual(name = "", values = c("Control Limits Adjusted for Flights" = "red", "Average Events Adjusted for Flights" = "blue", "Count of Events" = "black"))
-
+    scale_color_manual(name = "", values = scale_values)
+  
+  # Bring back warnings
+  options(warn = 0)
+  
   # Plot
   return(plot)
   }
