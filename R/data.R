8b23d293cb0fda24caf4a3de8786e49e725a0d59
Pericles Maranhao Neto <peconeto@gmail.com>
2018-06-01 13:38:26 -0400

    Dataset and spcChart updates
    1) Removed datasets that cannot be efficiently kept current: AirportCodes_20170404, AirportTimezones_20170404, and USA_DaylightSavings_20170404
    2) Updates to spcChart function:
    - Improved consistency of tidyverse function usage vs. base R
    - Removed run rules
    - Added ability to enable or disable LCL and UCL
    - Chart title now automatically uses filename when no title is provided
    3) Updated documentation

--- R/data.R
+++ R/data.R
@@ -11,39 +11,3 @@
 #' }
 #' @source \url{https://app.ntsb.gov/aviationquery/}
 "NTSBData_20170328"
-
-#' @title
-#' IATA and ICAO Airport Codes from 2017-04-04
-#' @description
-#' A list of global ICAO and IATA airport code from 2017-04-04.
-#' Useful for converting between the two since airport-related data is often available in one or the other format.
-#' @format A data frame with 30908 rows and 2 variables:
-#' \describe{
-#'   \item{IATA_Code}{3-letter IATA codes}
-#'   \item{ICAO_Code}{4-letter ICAO codes}
-#' }
-"AirportCodes_20170404"
-
-#' @title
-#' Airport Timezones from 2017-04-04
-#' @description
-#' A list of airport timezones from 2017-04-04.
-#' @format A data frame with 3527 rows and 2 variables:
-#' \describe{
-#'   \item{Code}{4-letter ICAO codes}
-#'   \item{Zulu_Offset}{Integer representing hours offset from Zulu (UTC)}
-#' }
-"AirportTimezones_20170404"
-
-#' @title
-#' 2010's USA Daylight Savings Dates from 2017-04-04
-#' @description
-#' A list of 2010-2019 USA daylight savings change dates from 2017-04-04.
-#' @format A data frame with 20 rows and 3 variables:
-#' \describe{
-#'   \item{Change_Name}{Name of change; either ST -> DST ("spring forward") or DST -> ST ("fall back")}
-#'   \item{Change_Value}{Integer representing changed time in hours (+1 or -1)}
-#'   \item{Date_Time}{Local daylight savings time change date and time}
-#' }
-#' @source \url{https://www.timeanddate.com/time/zone/usa/new-york}
-"USA_DaylightSavings_20170404"
--- R/spcChart.R
+++ R/spcChart.R
@@ -21,31 +21,28 @@
 #' ggplot2::ggsave(filename = "np-Chart.png", plot = spcChart(filename = "file.csv"), device = "png", path = getwd(), height = 7, width = 14, units = "in")
 #' @export
 spcChart <- function(filename,
-                     title = NA,
+                     title = basename(filename),
                      type = c("np", "p"),
                      group = c("monthly", "quarterly"),
                      enddate = floor_date(today(), "month") - 1, # Last day of previous month
-                     startdate = enddate - months(24) + 1) { # Go 24 months back
-  
+                     startdate = enddate - months(24) + 1, # Go 24 months back
+                     upper = TRUE,
+                     lower = TRUE) {
+
   # Remove warnings
   options(warn = -1)
-  
-  # Check for filename
-  if(is.null(filename)) {
-    stop("Filename argument is required")
-  }
-  
+
   # Required packages without exposing messages
   suppressMessages(require(lubridate))
   suppressMessages(require(tidyverse))
   suppressMessages(require(qcc))
-  
+
   # Read input files
   df <- suppressMessages(read_csv(filename))
 
   # Check for legitimate arguments
   if(!(is.data.frame(df)) | ncol(df) != 3) {
-    stop("Data file not recognized as a data.frame with three columns")
+    stop("Data file not recognized as a data.frame with three columns, or failed parsing")
   }
   if(!(group[1] %in% c("monthly", "quarterly"))) {
     stop("Grouping type not recognized")
@@ -56,44 +53,38 @@ spcChart <- function(filename,
   if(!type[1] %in% c("np", "p")) {
     stop("Only np or p chart types accepted")
   }
+  if(nrow(df) == 0) {
+    stop("Data.frame provided has no rows")
+  }
 
   # Clean up input data file
-  # [Re-]name columns based on position
+  # Name columns based on position
   df <- df %>%
     select(Date_Label_Month = 1,
            Events = 2,
            Operations = 3)
 
   # Convert date labels into R Date format, necessary for grouping and filtering
-  dates_list <- df %>%
-    select(Date_Label_Month) %>%
-    unlist() %>%
-    strsplit(",")
-
-  months_vector <- dates_list %>%
-    sapply("[", 1) %>%
-    unname()
-
-  years_vector <- dates_list %>%
-    sapply("[", 2) %>%
-    unname() %>%
-    as.integer()
-
-  dates_vector <- paste(years_vector, months_vector, "15", sep = "-") %>%
-    as.Date(format = "%Y-%b-%d") %>%
-    ceiling_date("month") - 1
-
   df <- df %>%
-    mutate(Date_Collected = dates_vector) %>%
+    separate(Date_Label_Month, c("Month", "Year", sep = ", "), remove = FALSE) %>%
+    select(-`, `) %>%
+    unite(Date_Collected, c(Year, Month), sep = "-") %>%
+    mutate(Date_Collected = paste(Date_Collected, "15", sep = "-")) %>%
+    mutate(Date_Collected = ymd(Date_Collected)) %>%
+    mutate(Date_Collected = ceiling_date(Date_Collected, "month") - 1) %>%
     mutate(Quarter_int = quarter(Date_Collected)) %>%
     mutate(Year_int = year(Date_Collected)) %>%
     mutate(Date_Label_Quarter = paste(paste("Q", Quarter_int, sep = ""),
-                                      Year_int, sep = ", ")) %>%
-    select(-Quarter_int, -Year_int)
+                                      Year_int, sep = ", "))# %>% select(-Quarter_int, -Year_int)
 
   # Apply date filter
   df <- df %>%
     filter(Date_Collected >= startdate, Date_Collected <= enddate)
+
+  # Check for any remaining values
+  if(nrow(df) == 0) {
+    stop("Date filter has removed all rows from data.frame")
+  }
 
   # Apply grouping
   if(group[1] == "monthly") {
@@ -106,81 +97,58 @@ spcChart <- function(filename,
   }
   if(group[1] == "quarterly") {
     df_grouped <- df %>%
-      group_by(Date_Label_Quarter) %>%
+      group_by(Date_Label_Quarter, Year_int, Quarter_int) %>%
       summarise(Events = sum(Events), Operations = sum(Operations)) %>%
       ungroup() %>%
+      arrange(Year_int, Quarter_int) %>%
       select(Date_Label = Date_Label_Quarter,
              Events = Events,
              Operations = Operations)
-
-    # Must re-order data.frame after grouping to preserve time series order
-    dates_list_ordering <- df_grouped %>%
-      select(Date_Label) %>%
-      unlist() %>%
-      strsplit(", ")
-
-    quarters_vector_ordering <- dates_list_ordering %>%
-      sapply("[", 1) %>%
-      unname()
-
-    years_vector_ordering <- dates_list_ordering %>%
-      sapply("[", 2) %>%
-      unname() %>%
-      as.integer()
-
-    df_grouped <- df_grouped %>%
-      mutate(Year_int = years_vector_ordering) %>%
-      mutate(Quarter_int = quarters_vector_ordering) %>%
-      arrange(Year_int, Quarter_int) %>%
-      select(-Year_int, -Quarter_int)
   }
 
-  # Create quality control chart
-  # Collect variables for QCC calculation
-  observations <- df_grouped %>%
-    select(Events) %>%
-    unlist() %>%
-    unname()
-
-  labels <- df_grouped %>%
-    select(Date_Label) %>%
-    unlist() %>%
-    unname()
-
-  sizes <- df_grouped %>%
-    select(Operations) %>%
-    unlist() %>%
-    unname()
-
-  # Chart type
+  # Create chart labels based on type
   chartType <- type[1]
   centerLabel <- case_when(chartType == "np" ~ "Average Events Adjusted for Flights",
                            chartType == "p" ~ "Average Events")
   yAxisLabel <- case_when(chartType == "np" ~ "Count of Events",
                            chartType == "p" ~ "Proportion of Events")
-  
-  # Calculate QCC values
-  qcc_calc <- qcc(data = observations, type = chartType, sizes = sizes, plot = FALSE)
 
-  qcc_df <- data.frame(labels = labels,
-                           center = qcc_calc$center,
-                           values = qcc_calc$statistics,
-                           LCL = qcc_calc$limits[,1],
-                           UCL = qcc_calc$limits[,2],
-                           beyond_limits = 0,
-                           stringsAsFactors = FALSE)
+  # Calculate QCC values
+  qcc_calc <- qcc(data = pull(df_grouped, Events),
+                  type = chartType,
+                  sizes = pull(df_grouped, Operations),
+                  plot = FALSE)
+
+  qcc_df <- tibble(labels = pull(df_grouped, Date_Label),
+                   center = qcc_calc$center,
+                   values = qcc_calc$statistics,
+                   LCL = qcc_calc$limits[,1],
+                   UCL = qcc_calc$limits[,2],
+                   beyond_limits = 0)
 
   # Convert labels into factor so ggplot will know to order it non-alphabetically
-  qcc_df$labels <- factor(qcc_df$labels,
-                              levels = unique(qcc_df$labels))
+  qcc_df <- qcc_df %>%
+    mutate(labels = factor(labels, levels = unique(pull(qcc_df, labels))))
 
-  # Update data.frame with rows where runs rules were exceeded
-  runs_index <- unname(unlist(qcc_calc$violations[2]))
-  qcc_df[runs_index, "beyond_limits"] <- 2
+  # Disable UCL or LCL depending on function input
+  if(upper == FALSE) {
+    qcc_df <- qcc_df %>%
+      mutate(UCL = NA)
+  }
+  if(lower == FALSE) {
+    qcc_df <- qcc_df %>%
+      mutate(LCL = NA)
+  }
 
   # Update data.frame with rows where limit was exceeded
-  beyond_limits_index <- unname(unlist(qcc_calc$violations[1]))
-  qcc_df[beyond_limits_index, "beyond_limits"] <- 1
+  if(upper == TRUE) {
+    qcc_df <- qcc_df %>%
+      mutate(beyond_limits = if_else(values > UCL, 1, 0))
+  }
+  if(lower == TRUE) {
+    qcc_df <- qcc_df %>%
+      mutate(beyond_limits = if_else(values < LCL, 1, 0))
+  }
 
   # scale_color_manual values
   if(chartType == "np") {
@@ -189,26 +157,39 @@ spcChart <- function(filename,
   if(chartType == "p") {
     scale_values <- c("Control Limits Adjusted for Flights" = "red", "Average Events" = "blue", "Proportion of Events" = "black")
   }
-  
+
   # Create ggplot
-  plot <- ggplot(qcc_df) +
+  plot <- ggplot(data = qcc_df) +
     labs(title = title, x = "Dates", y = yAxisLabel[1]) +
     theme(axis.text = element_text(size = 8),
           axis.title = element_text(size = 14),
           title = element_text(size = 18),
           legend.text = element_text(size = 12),
-          legend.position = "bottom") +
-    geom_step(aes(x = labels, y = LCL, color = "Control Limits Adjusted for Flights"), linetype = 2, group = 1) +
-    geom_step(aes(x = labels, y = UCL, color = "Control Limits Adjusted for Flights"), linetype = 2, group = 1) +
+          legend.position = "bottom")
+
+  # Lower boundary step later
+  if(lower == TRUE) {
+      plot <- plot +
+        geom_step(aes(x = labels, y = LCL, color = "Control Limits Adjusted for Flights"), linetype = 2, group = 1)
+      }
+
+  # Upper boundary step later
+  if(upper == TRUE) {
+      plot <- plot +
+        geom_step(aes(x = labels, y = UCL, color = "Control Limits Adjusted for Flights"), linetype = 2, group = 1)
+      }
+
+  # Finish creating ggplot
+  plot <- plot +
     geom_step(aes(x = labels, y = center, color = centerLabel[1]), linetype = 2, group = 1) +
     geom_line(aes(x = labels, y = values, color = yAxisLabel[1]), group = 1) +
     geom_point(aes(x = labels, y = values, fill = factor(beyond_limits)), shape = 21 , color = "transparent" , size = 4) +
-    scale_fill_manual(values = c("0" = "black", "1" = "red", "2" = "yellow"), guide = FALSE) +
+    scale_fill_manual(values = c("0" = "black", "1" = "red"), guide = FALSE) +
     scale_color_manual(name = "", values = scale_values)
-  
+
   # Bring back warnings
   options(warn = 0)
-  
+
   # Plot
   return(plot)
   }
